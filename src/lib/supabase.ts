e812a1bed7e1983782c6670c5711e34f322257f2
Jeremy Schaffer <jeremys@codenexus.org>
2025-06-04 18:52:07 -0700

    fix: remove unused imports in supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,5 @@
-import { createBrowserClient, createServerClient, isBrowser } from '@supabase/ssr'
+import { createBrowserClient } from '@supabase/ssr'
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
-import type { LayoutLoad } from './$types'
 
 export const createSupabaseLoadClient = (fetch: typeof globalThis.fetch) => {
   return createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
